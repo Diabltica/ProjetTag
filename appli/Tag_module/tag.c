cebb3f5fe9067f3d52986e23526723e25d9cc820
Thomas LE BERRE <thomas.le-berre@saintaubinlasalle.fr>
2023-04-14 11:36:25 +0200

    branche propre

--- appli/Tag_module/tag.c
+++ appli/Tag_module/tag.c
@@ -31,6 +31,7 @@ struct tag_t{
 	State state;};
 
 
+
 // PRIVATE FUNCTIONS DEFINITIONS -----------------------------------------------
 
 
@@ -42,6 +43,7 @@ extern Tag* Tag_new(uint32_t mem_address)
 	Tag* this;
 	this=(Tag* )malloc(sizeof(Tag));
 
+
 	this->state = S_DEATH;
 	this->flash_address = mem_address;
 	this->ram_buffer = EMPTY_SERIAL;
@@ -49,8 +51,28 @@ extern Tag* Tag_new(uint32_t mem_address)
 	return this;
 }
 
-extern setSerial(uint32_t){
+extern void Tag_start(Tag *this){
+
 }
+
+extern void Tag_setSerial(Tag *this, uint32_t serial){
+
+}
+extern void Tag_printSerial(Tag* this){
+
+}
+extern void Tag_sleep(Tag *this){
+
+}
+extern void Tag_storeMem(Tag* this){
+
+}
+extern void Tag_loadMem(Tag* this){
+	this->ram_buffer = LOADED_SERIAL;
+}
+
+extern void Tag_stop(Tag *this){
+
 }
 
 extern void Tag_free(Tag *this){
